a64f8e59caf63c3c48e339990e5d2de77b016d6a
borismarkovsv73 <markovboris3@gmail.com>
2025-09-17 19:45:56 +0200

    [feat]: added function for loading moves

--- aizul/model/src/main/java/com/ftn/sbnz/model/utils/JsonLoader.java
+++ aizul/model/src/main/java/com/ftn/sbnz/model/utils/JsonLoader.java
@@ -3,10 +3,16 @@ package com.ftn.sbnz.model.utils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import com.ftn.sbnz.model.models.GameState;
+import com.ftn.sbnz.model.models.Move;
 
 public class JsonLoader {
     public static GameState loadGameState(String filePath) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(new File(filePath), GameState.class);
+    }
+
+    public static Move loadMove(String filePath) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(new File(filePath), Move.class);
     }
 }
